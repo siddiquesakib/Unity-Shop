b6c03a0bb7cbb7ead4669114c54b7a5c818f8fff
Abu Abdullah Mohammed Iqram <aamiqram24@gmail.com>
2026-02-15 11:22:25 +0600

    pushing my branch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <p>Hello world</p>
+      <p>Hello world</p>
+      <p>HI! I'm Iqram</p>
     </div>
   );
 }
