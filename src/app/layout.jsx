0832dfdc8a8f7000f8c84a6a5615741bdbbd0dc7
siddiquesakib <dmsakib1122@gmail.com>
2026-03-01 23:23:41 +0600

    chore(layout): update root layout structure

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
+import AnnouncementBar from "@/components/common/AnnouncementBar";
 import { AuthProvider } from "@/contexts/AuthContext";
 import NextAuthProvider from "@/components/providers/NextAuthProvider";
 import { CartProvider } from "@/contexts/CartContext";
@@ -31,7 +32,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f7f6f3]`}
       >
         <LanguageProvider>
           <CurrencyProvider>
@@ -40,6 +41,7 @@ export default function RootLayout({ children }) {
                 <SocketProvider>
                   <NotificationProvider>
                     <CartProvider>
+                      <AnnouncementBar />
                       <Navbar />
                       <NotificationListener />
                       <Toaster position="top-right" />
